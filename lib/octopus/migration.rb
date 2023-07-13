9bc54c6319cf6fd9ca30b19ccb8870b9bc4c6f89
Matthew Hirst <hirst.mat@gmail.com>
2023-07-13 19:32:12 +0300

    Disable octopus migrations.

--- lib/octopus/migration.rb
+++ lib/octopus/migration.rb
@@ -62,37 +62,40 @@ end
 module Octopus
   module Migrator
     def self.included(base)
-      unless Octopus.atleast_rails52?
-        base.extend(ClassMethods)
+      # Disabling all migration help
+      if false
+        unless Octopus.atleast_rails52?
+          base.extend(ClassMethods)
 
-        base.class_eval do
-          class << self
-              alias_method :migrate_without_octopus, :migrate
-              alias_method :migrate, :migrate_with_octopus
-              
-              alias_method :up_without_octopus, :up
-              alias_method :up, :up_with_octopus
+          base.class_eval do
+            class << self
+                alias_method :migrate_without_octopus, :migrate
+                alias_method :migrate, :migrate_with_octopus
 
-              alias_method :down_without_octopus, :down
-              alias_method :down, :down_with_octopus
+                alias_method :up_without_octopus, :up
+                alias_method :up, :up_with_octopus
 
-              alias_method :run_without_octopus, :run
-              alias_method :run, :run_with_octopus
+                alias_method :down_without_octopus, :down
+                alias_method :down, :down_with_octopus
 
-              alias_method :rollback_without_octopus, :rollback
-              alias_method :rollback, :rollback_with_octopus
+                alias_method :run_without_octopus, :run
+                alias_method :run, :run_with_octopus
+
+                alias_method :rollback_without_octopus, :rollback
+                alias_method :rollback, :rollback_with_octopus
+            end
           end
         end
+
+        base.send :alias_method, :run_without_octopus, :run
+        base.send :alias_method, :run, :run_with_octopus
+
+        base.send :alias_method, :migrate_without_octopus, :migrate
+        base.send :alias_method, :migrate, :migrate_with_octopus
+
+        base.send :alias_method, :migrations_without_octopus, :migrations
+        base.send :alias_method, :migrations, :migrations_with_octopus
       end
-
-      base.send :alias_method, :run_without_octopus, :run
-      base.send :alias_method, :run, :run_with_octopus
-
-      base.send :alias_method, :migrate_without_octopus, :migrate
-      base.send :alias_method, :migrate, :migrate_with_octopus
-
-      base.send :alias_method, :migrations_without_octopus, :migrations
-      base.send :alias_method, :migrations, :migrations_with_octopus
     end
     if Octopus.atleast_rails52?
       ### Post RAILS 5.2 Migration methods
